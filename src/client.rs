7bf374aed68cfd35af3387cbdd2a52ad5a7f575d
Blxee <aminedev57@gmail.com>
2026-05-13 22:25:01 +0100

    Added: basic chat capability to server

--- src/client.rs
+++ src/client.rs
@@ -1,9 +1,29 @@
 use serde::{Deserialize, Serialize};
 use serde_json::to_string;
-use tokio::net::TcpStream;
+use tokio::{
+    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader, stdin},
+    net::TcpStream,
+};
 
 pub(crate) async fn client_main(host_socket: TcpStream) {
-    let js = to_string(&ClientProtocol::Join);
+    println!("connected to host");
+
+    let (mut reader, mut writer) = host_socket.into_split();
+
+    tokio::spawn(async move {
+        let mut buf = [0u8; 1024];
+
+        while let Ok(1..) = reader.read(&mut buf).await {
+            println!("{}", String::from_utf8_lossy(&buf));
+        }
+    });
+
+    let mut stdin_buf_reader = BufReader::new(stdin());
+    let mut buf = String::new();
+    while let Ok(_) = stdin_buf_reader.read_line(&mut buf).await {
+        writer.write_all(buf.as_bytes()).await.unwrap();
+        buf.clear();
+    }
 }
 
 #[derive(Serialize, Deserialize)]
--- src/host.rs
+++ src/host.rs
@@ -1,1 +1,42 @@
-pub(crate) async fn host_main() {}
+use tokio::{
+    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader, stdin},
+    net::{TcpListener, UdpSocket},
+};
+
+use crate::{CONNECT_PORT, SEARCH_PORT};
+
+pub(crate) async fn host_main() {
+    tokio::spawn(async {
+        let search_socket = UdpSocket::bind(("0.0.0.0", SEARCH_PORT)).await.unwrap();
+
+        let mut buf = [0u8; 4];
+        while let Ok((_, client_addr)) = search_socket.recv_from(&mut buf).await {
+            if buf == *b"PING" {
+                search_socket.send_to(b"PONG", client_addr).await.unwrap();
+            }
+        }
+    });
+
+    let listener = TcpListener::bind(("0.0.0.0", CONNECT_PORT)).await.unwrap();
+
+    while let Ok((client_socket, client_addr)) = listener.accept().await {
+        println!("connected to client");
+
+        let (mut reader, mut writer) = client_socket.into_split();
+
+        tokio::spawn(async move {
+            let mut buf = [0u8; 1024];
+
+            while let Ok(1..) = reader.read(&mut buf).await {
+                println!("{}", String::from_utf8_lossy(&buf));
+            }
+        });
+
+        let mut stdin_buf_reader = BufReader::new(stdin());
+        let mut buf = String::new();
+        while let Ok(_) = stdin_buf_reader.read_line(&mut buf).await {
+            writer.write_all(buf.as_bytes()).await.unwrap();
+            buf.clear();
+        }
+    }
+}
