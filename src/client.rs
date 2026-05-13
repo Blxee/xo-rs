401388dfa4929b4a1f3fde4c0ac0788907a84865
Blxee <aminedev57@gmail.com>
2026-05-13 14:24:23 +0100

    Added: serde as dependency

--- src/client.rs
+++ src/client.rs
@@ -1,3 +1,13 @@
+use serde::{Deserialize, Serialize};
+use serde_json::to_string;
 use tokio::net::TcpStream;
 
-pub(crate) async fn client_main(host_socket: TcpStream) {}
+pub(crate) async fn client_main(host_socket: TcpStream) {
+    let js = to_string(&ClientProtocol::Join);
+}
+
+#[derive(Serialize, Deserialize)]
+enum ClientProtocol {
+    Join,
+    Leave,
+}
