6634e46f08e50a4a9881621e3f4709c160612f9d
Blxee <aminedev57@gmail.com>
2026-05-14 11:54:50 +0100

    Changed: the host now keep trying ports until it finds empty ones

--- src/host.rs
+++ src/host.rs
@@ -6,37 +6,55 @@ use tokio::{
 use crate::{CONNECT_PORT, SEARCH_PORT};
 
 pub(crate) async fn host_main() {
-    tokio::spawn(async {
-        let search_socket = UdpSocket::bind(("0.0.0.0", SEARCH_PORT)).await.unwrap();
+    let mut search_port = SEARCH_PORT;
+    let search_socket = loop {
+        match UdpSocket::bind(("0.0.0.0", search_port)).await {
+            Ok(socket) => break socket,
+            Err(_) => (),
+        }
+        search_port += 1;
+    };
 
-        let mut buf = [0u8; 4];
-        while let Ok((_, client_addr)) = search_socket.recv_from(&mut buf).await {
-            if buf == *b"PING" {
-                search_socket.send_to(b"PONG", client_addr).await.unwrap();
+    let mut connect_port = CONNECT_PORT;
+    let listener = loop {
+        match TcpListener::bind(("0.0.0.0", connect_port)).await {
+            Ok(listener) => break listener,
+            Err(_) => (),
+        }
+        connect_port += 1;
+    };
+    let connect_port_msg = connect_port.to_string();
+
+    let mut buf = [0u8; 4];
+    let (client_socket, client_addr) = loop {
+        match search_socket.recv_from(&mut buf).await {
+            Ok((_, client_addr)) if buf == *b"PING" => {
+                search_socket
+                    .send_to(connect_port_msg.as_bytes(), client_addr)
+                    .await
+                    .unwrap();
+                break listener.accept().await.unwrap();
             }
+            _ => continue,
+        }
+    };
+
+    println!("client {} connected..", client_addr);
+
+    let (mut reader, mut writer) = client_socket.into_split();
+
+    tokio::spawn(async move {
+        let mut buf = [0u8; 1024];
+
+        while let Ok(1..) = reader.read(&mut buf).await {
+            println!("{}", String::from_utf8_lossy(&buf).trim());
         }
     });
 
-    let listener = TcpListener::bind(("0.0.0.0", CONNECT_PORT)).await.unwrap();
-
-    while let Ok((client_socket, client_addr)) = listener.accept().await {
-        println!("connected to client");
-
-        let (mut reader, mut writer) = client_socket.into_split();
-
-        tokio::spawn(async move {
-            let mut buf = [0u8; 1024];
-
-            while let Ok(1..) = reader.read(&mut buf).await {
-                println!("{}", String::from_utf8_lossy(&buf));
-            }
-        });
-
-        let mut stdin_buf_reader = BufReader::new(stdin());
-        let mut buf = String::new();
-        while let Ok(_) = stdin_buf_reader.read_line(&mut buf).await {
-            writer.write_all(buf.as_bytes()).await.unwrap();
-            buf.clear();
-        }
+    let mut stdin_buf_reader = BufReader::new(stdin());
+    let mut buf = String::new();
+    while let Ok(_) = stdin_buf_reader.read_line(&mut buf).await {
+        writer.write_all(buf.as_bytes()).await.unwrap();
+        buf.clear();
     }
 }
